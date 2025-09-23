1c52e161580a1ddd5e14400d384c00837fa4e144
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2025-09-23 16:44:40 +0300

    Update Hero.jsx
    Added a transparent white overlay

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -11,4 +11,12 @@ const Hero = () => {
         //Open phone dialer
         window.location.href='tel:+254712345678';
     };
+
+return (
+<section id="home"
+className="pt-20 bg-gradient-to-br from-orange-50 via-red-50 to-yellow-50 relative overflow-hidden">
+
+{/* Background overlay*/}
+<div className="absolute insert-0 bg-white/20"></div>
+);
 }
